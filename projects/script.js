ad91f18c0bd7bf78ae1a3607efe7f60a5164ff89
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-05-24 01:26:57 +0530

    commit

--- projects/script.js
+++ projects/script.js
@@ -20,11 +20,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Aakrati Gupta";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Come Back";
             $("#favicon").attr("href", "/assets/images/favhand.png");
         }
     });
